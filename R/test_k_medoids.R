9f1c4c02e6f920c040fcfbc6a2acfea420a170a5
fchrubasik <58042054+fchrubasik@users.noreply.github.com>
2020-08-14 15:31:55 +0200

    Update test_k_medoids.R

--- R/test_k_medoids.R
+++ R/test_k_medoids.R
@@ -1,6 +1,7 @@
 context("test the k-medoids algorithm")
 
 data <- matrix(c(1,1.1,1,1,2,2,2,2.1), ncol=4)
+data2 <- NULL
 
 test_that("k-medoids finds the correct clusters for k = 2", {
   expect_equal(6, sum(attributes(k_medoids(data, 2))[[2]]))
@@ -12,4 +13,8 @@ test_that("k-medoids finds the correct clusters for k = 1", {
 
 test_that("k-medoids finds the correct clusters for k = 4", {
   expect_equal(10, sum(attributes(k_medoids(data, 4))[[2]]))
+})
+
+test_that("wrong input results in error for k-medoids", {
+  expect_error(k_medoids(data2, 2))
 })
