6547db79731c4299340221b8c53a7fd4dcfb20d5
Georg Weisert <weisert@stud.uni-heidelberg.de>
2020-08-17 18:46:08 +0200

    fix bug in OPTICS; add cluster_by_reachability function; extend reachability plot function
    OPTICS: core-distance was incorrectly calculated
    cluster_by_reachability: extract clusters from the reachability data
    using a given threshold
    reachability_plot: add option to hide noise in the reachability plot -
    noise is simply shown as reachability 0. add the option to display a
    given threshold

--- R/optics.R
+++ R/optics.R
@@ -13,7 +13,7 @@
 #' @examples
 #' data <- matrix(c(1,1.1,1,1,2,2,2,2.1), ncol=4)
 #' DBSCAN(data, .2, 1)
-OPTICS <- function (data, eps, minPts, distanceFunction = euclidean_distance) {
+OPTICS <- function (data, eps, minPts, distanceFunction = euclidean_distance, threshold=NULL) {
   stopifnot( "data has to be a matrix. columns are vectors." = is.matrix(data));
   stopifnot( "eps has to be a positive number" = eps > 0);
   stopifnot( "minPts has to be a positive integer." = as.integer(minPts) > 0);
@@ -26,8 +26,8 @@ OPTICS <- function (data, eps, minPts, distanceFunction = euclidean_distance) {
     P <- data[, idx];
     distances <- apply(data, 2, function(D){ distanceFunction(P, D) })
 
-    neighbors <- seq(ncol(data))[distances < eps];
-    attr(neighbors, "core-distance") <- min(distances[-idx])
+    neighbors <- seq(n)[distances <= eps];
+    attr(neighbors, "core-distance") <- distances[order(distances)[minPts]];
     return(neighbors)
   }
 
@@ -42,6 +42,7 @@ OPTICS <- function (data, eps, minPts, distanceFunction = euclidean_distance) {
         } else {
           if (new_reach_dist < attr(data, "reachability-distance")[neighbor]) {
             attr(data, "reachability-distance")[neighbor] <<- new_reach_dist;
+            in_seeds[neighbor] <<- TRUE;
           }
         }
       }
@@ -51,7 +52,6 @@ OPTICS <- function (data, eps, minPts, distanceFunction = euclidean_distance) {
   attr(data, "visited") <- rep(FALSE, n);
   attr(data, "reachability-distance") <- rep(-1, n);
   attr(data, "core-distance") <- rep(-1, n);
-  # attr(data, "cluster") <- rep(-1, n);
 
   for (p_idx in seq(n)) {
     if (attr(data, "visited")[p_idx] == FALSE) {
@@ -85,23 +85,85 @@ OPTICS <- function (data, eps, minPts, distanceFunction = euclidean_distance) {
   attr(data, "visited") <- NULL;
   attr(data, "ordering") <- o;
 
+  if (!missing(threshold)) {
+    data <- cluster_by_reachability(data, threshold);
+  }
+
   return(data)
 }
 
-reachability_plot <- function (data) {
+
+#' Reachabilty Plot
+#' @description Generates a reachability plot for data resulting from the OPTICS function.
+#'
+#' @param data matrix; data has to be the result of a previous run of the OPTICS algorithm.
+#' @param threshold numeric; show the given threshold in the plot, optinal.
+#'
+#' @return returns the input data untouched
+#' @export
+#'
+#' @examples
+#' data <- generate_2d_cluster(200);
+#' data <- OPTICS(data, .2, 25);
+#' reachability_plot(data, .15);
+reachability_plot <- function (data, threshold=NULL) {
   n <- ncol(data);
-  x <- seq(n);
+  noise_indices <- seq(n)[attr(data, "reachability-distance") == -1];
+  numNoise <- length(noise_indices);
+
   y <- c();
-  for (i in x) {
+  for (i in seq(n)) {
     idx <- attr(data, "ordering")[i];
+
+    if (idx %in% noise_indices) next; # skip if noise
+
     reach_dist <- attr(data, "reachability-distance")[idx];
-    if (reach_dist != -1) {
-      y <- c(y, reach_dist);
-    } else {
-      y <- c(y, 0);
+    y <- c(y, reach_dist);
+  }
+
+  plot(seq(n - numNoise), y, type="h");
+  if (!missing(threshold)) abline(h=threshold, col="red");
+
+  return(data);
+}
+
+
+#' Cluster by Reachability
+#' @description Extract cluster via a reachability threshold. Using the correct threshold extracts the DBSCAN clustering. This correct threshold might for example be guessed by looking at the reachability plot with each valley being a cluster.
+#'
+#' @param data matrix; input vectors, with each column of the matrix being a vector. \code{nrow(data)} is the ector dimension.
+#' @param threshold double threshold used to determine clusters
+#'
+#' @return returns \code{data} with an addition \code{"cluster"} attribute containing the clusters. Noise is marked as -1.
+#' @export
+#'
+#' @examples
+#' data <- generate_2d_cluster(200);
+#' data <- OPTICS(data, .2, 25);
+#' cluster_by_reachability(data, .15);
+cluster_by_reachability <- function (data, threshold) {
+  stopifnot("threshold has to be numeric!" = is.numeric(threshold));
+
+  n <- ncol(data)
+  cluster <- c()
+  curCluster <- 0;
+  prevWasNoise <- TRUE;
+  for (i in seq(n)) {
+    idx <- attr(data, "ordering")[i];
+    rd <- attr(data, "reachability-distance")[idx];
+    if (rd >= threshold || rd == -1) {
+      cluster[idx] <- -1;
+      prevWasNoise <- TRUE;
+    } else if (rd < threshold) {
+      if (prevWasNoise) {
+        curCluster <- curCluster + 1;
+        prevWasNoise <- FALSE;
+      }
+      cluster[idx] <- curCluster;
     }
   }
 
-  plot(x,y, type="h");
-}
+  attr(data, "cluster") <- cluster;
 
+  return(data);
+}
