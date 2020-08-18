cf8e5435d67a598df3e9b8089eec4df412d76d4f
Ahmad Fadl <abohmaid@windowslive.com>
2020-08-18 20:40:28 +0200

    Cluster attribute angepasst

--- R/kmeanalgorithm.R
+++ R/kmeanalgorithm.R
@@ -1,9 +1,9 @@
 
-#' @description chose random centers for clusters.
+#' @description chose random centers for cluster.
 #'   it will only be used internely in kmean clustering algorithm.
 #' @param data matrix; matrix columns are (mathematical) vectors. Input data.
 #'
-#' @param k double; number of clusters
+#' @param k double; number of cluster
 #'
 #'
 #'
@@ -24,12 +24,12 @@ m <- function(mydata,k){
 #' @description kmean algorithm see Richter19 9.10
 #' @param data matrix; matrix columns are (mathematical) vectors. Input data.
 #'
-#' @param k double; number of clusters
+#' @param k double; number of cluster
 #'
 #'
 #'
 #' @return matrix; with k columns with additionall attribuets
-#'               1 clusters : vector of values between 1 to k has length ncol(mydata) .
+#'               1 cluster : vector of values between 1 to k has length ncol(mydata) .
 #'               2 centers  : matrix has k col in each col is saved the center of each cluster.
 #' @export
 #'
@@ -40,14 +40,14 @@ kmean <- function(mydata,k=3,distanceFunction=euclidean_norm_squared) {
   stopifnot( "data has to be a matrix. columns are vectors." = is.matrix(mydata));
   stopifnot("data cannot be empty!" = length(mydata) > 0)
   zent = m(mydata,k)
-  mydata= `attr<-`(mydata,"clusters",rep(1,ncol(mydata)))
+  mydata= `attr<-`(mydata,"cluster",rep(1,ncol(mydata)))
   iter = 0
   while (TRUE) {
     for (i in seq_along(mydata[1,])) {
       for (j in 1:k) {
         #print(as.integer(attr(mydata[[i]],'cluster')))
-        if(distanceFunction(mydata[,i]-zent[,attr(mydata,"clusters")[i]]) > distanceFunction(mydata[,i]-zent[,j]))
-          {attr(mydata,"clusters")[i]=j}
+        if(distanceFunction(mydata[,i]-zent[,attr(mydata,"cluster")[i]]) > distanceFunction(mydata[,i]-zent[,j]))
+          {attr(mydata,"cluster")[i]=j}
       }
     }
     #cat("this ist the time",toString(iter))
@@ -56,7 +56,7 @@ kmean <- function(mydata,k=3,distanceFunction=euclidean_norm_squared) {
     for (i in seq_along(zent[1,])) {
       n=0
       for (j in seq_along(mydata[1,])){
-        if(all(zent[,attr(mydata,"clusters")[j]]==zent[,i])){
+        if(all(zent[,attr(mydata,"cluster")[j]]==zent[,i])){
           n=n+1
           newzent[,i]= newzent[,i]+ mydata[,j]
         }
@@ -75,7 +75,7 @@ kmean <- function(mydata,k=3,distanceFunction=euclidean_norm_squared) {
     zent = newzent
     iter = iter +1
     #if (iter>200){
-     # print("es gibt keine eindeutige Zetren von clusters")
+     # print("es gibt keine eindeutige Zetren von cluster")
       #break
       #}
   }
