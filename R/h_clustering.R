aac66f8a1fbcb3dae8216131e0c9b49b28f25e3a
Mitodru Niyogi <mitodru.niyogi@gmail.com>
2020-08-19 14:56:13 +0200

    Add Roxygen comments for Hierarchical  clustering

--- R/h_clustering.R
+++ R/h_clustering.R
@@ -1,6 +1,19 @@
-# Distance function to compute a full Euclidean distance matrix for
-# the input matrix x to measure the dissimilarities. The output[i,j] gives 
-# the Euclidean distance between rows x[i,] and x[j,].
+#' Hierarchical Clustering Implementation
+#' Distance function to compute a full Euclidean distance matrix for
+#' the input matrix x to measure the dissimilarities. The output[i,j] gives
+#' the Euclidean distance between rows x[i,] and x[j,].
+#' dissimialrity
+#'
+#' @description calculates all dissimilarities from a data point
+#'
+#' @param x matrix; columns are vectors
+#'
+#' @return vector of all dissimilarities
+#' @export
+#'
+#' @examples
+#' data <- matrix(c(1,1.1,1,1,2,2,2,2.1), ncol=4)
+#' distance(data)
 distance = function(x)
 {
   x = as.matrix(x)
@@ -8,10 +21,19 @@ distance = function(x)
   sqrt(abs(u + t(u) - 2 * x %*% t(x)))
 }
 
-# The function returns a vector giving the permutation of the original
-# observations suitable 
-# for plotting, in the sense that a cluster plot using this ordering and matrix
+#' Hieraerchical clusters order
+#'
+#' @description  The function returns a vector giving the permutation of the original
+# observations suitable for plotting, in the sense that a cluster plot using this ordering and matrix
 # will not have crossings of the branches.
+#' @param mat matrix; an n−1 by 2 matrix
+#'
+#' @return vector in ordered manner given the permutation of the original observations for plotting
+#' @export
+#'
+#' @examples
+#' data <- matrix(c(1,1.1,1,1,2,2,2,2.1), ncol=4)
+#' hclust_order(distance(data))
 hclust_order = function(mat)
 {
   N = nrow(mat) + 1
@@ -42,25 +64,40 @@ hclust_order = function(mat)
   -hclust_order
 }
 
-#hierarchical clustering implementation.
-# be used like hclust(d, method).
+#' Hierarchical clustering implementation. It can bee used like original hclust(d, method).
+#' Hierarchical Clustering over a Matrix's Column-Vectors.
+#'
+#' @description Performs the agglomerative hierarchical clustering algorithm describe in "Definition 10.3.2" in reference James19
+#' @references Gareth James, Daniela Witten, Trevor Hastie, Robert Tibshirani. (2013). An Introduction to Statistical Learning with Applications in R.
+#'
+#' @param d matrix; input matrix columns are vectors
+#' @param method string; linkage method usage
+#'
+#' @return  the list structure object(labels, call, method, order, height, merge, dist.method) as similar to the output from hclust.
+#' @export
+#'
+#' @examples
+#' data <- distance(USArrests)
+#' data_clust <- hierclust(data, method="single")
 hierclust = function(d, method=c("single","complete","average","median"))
 {
+  stopifnot("data cannot be empty!" = length(d) > 0);
+  stopifnot("data must be a matrix!" = is.matrix(d) == TRUE);
   if(!is.matrix(d)) d = as.matrix(d)
   # defining clustering functions
   method_cluster = switch(match.arg(method),
-                     single   = min,
-                     complete = max,
-                     average  = mean,
-                     median  = median)
+                          single   = min,
+                          complete = max,
+                          average  = mean,
+                          median  = median)
   Nrows = nrow(d)
   diag(d)=Inf
   n = -(1:Nrows)                       # for group membership
   m = matrix(0,nrow=Nrows-1, ncol=2)   # merge output of hclust function
-  # an n−1 by 2 matrix. Row i of merge describes the merging of clusters at step 
+  # an n−1 by 2 matrix. Row i of merge describes the merging of clusters at step
   # i of the clustering.
   # If an element j in the row is negative, then observation -j was merged
-  # at this stage. If j is positive then the merge was with the cluster 
+  # at this stage. If j is positive then the merge was with the cluster
   # formed at the (earlier) stage j of the algorithm.
   # Thus negative entries in merge indicate agglomerations of singletons,
   # and positive entries indicate agglomerations of non-singletons.
@@ -77,14 +114,14 @@ hierclust = function(d, method=c("single","complete","average","median"))
     # order each m[j,] pair
     p = p[order(p)]
     m[j,] = p
-    # Agglomerate the previous groups and the current pair 
+    # Agglomerate the previous groups and the current pair
     # into the current jth group
     # (Agglomerative clustering)
     grp = c(i, which(n %in% n[i[1,n[i]>0]]))
     n[grp] = j
     # Applying chosen method distance to replacement distances
     r = apply(d[i,],2,method_cluster)
-    # Set the pointer to the next minimum distance, 
+    # Set the pointer to the next minimum distance,
     # excluding current one by modifying
     # the distance matrix
     d[min(i),] = d[,min(i)] = r
@@ -92,7 +129,7 @@ hierclust = function(d, method=c("single","complete","average","median"))
     d[max(i),] = d[,max(i)] = Inf
   }
   # Return the object as similar to the output from hclust.
-  
+
   # labels for each of the objects being clustered
   # call which produced the result
   # method is the cluster method used
@@ -100,19 +137,11 @@ hierclust = function(d, method=c("single","complete","average","median"))
   # suitable for plotting, in the sense that a cluster plot using
   # this ordering and matrix merge will not have crossings of the branches
   # height is a set of n−1 real values (non-decreasing for ultrametric trees).
-  # The clustering height is the value of the criterion associated 
+  # The clustering height is the value of the criterion associated
   # with the clustering method for the particular agglomeration.
   # merge is the n-1 by 2 matrix
   structure(list(merge = m, height = h, order = hclust_order(m),
-                 labels = rownames(d), method = method, 
-                 call = match.call(), dist.method = "euclidean"), 
+                 labels = rownames(d), method = method,
+                 call = match.call(), dist.method = "euclidean"),
             class = "hclust")
 }
-
-# Plot implemented hierclust() and defined R function hclust()
-# R defined hclust() function for hierarchical clustering
-h = hclust(dist(USArrests),method="single")
-# Our implemented agglomerative hierarchical clustering method
-h_clust = hierclust(distance(USArrests), method="single")
-plot(h)
-plot(h_clust)
