43db6c39be3784531894b5d1ab890f737950d961
Georg Weisert <weisert@stud.uni-heidelberg.de>
2020-08-21 09:37:28 +0200

    final changes to the presentation and live demo stuff

--- vignettes/live_demo/image_demo.R
+++ vignettes/live_demo/image_demo.R
@@ -14,7 +14,7 @@ plot_rgb_matrix <- function (data, w ,h) {
       col <- c(col, rgb(cur[1], cur[2], cur[3], 1))
     }
   }
-  plot(x,y,pch=15,col=col)
+  plot(x,y,pch=15,col=col, ann=FALSE, xaxt='n', yaxt='n')
 }
 
 plot_clustered_rgb_vectors <- function(data, w, h, cl=1, bgCol=rgb(1,1,1,1)) {
@@ -57,20 +57,21 @@ convert_imager_to_matrix <- function (image) {
   return(result);
 }
 
-original <- imager::load.image("C:/Users/Georg/Downloads/3c054bb7166ffd4cc8a7da20fd113e1e.jpg")
+original <- imager::load.image(paste(getwd(),"/vignettes/live_demo/sample_image.jpg", sep = ""))
 cropped <- imager::crop.borders(original, 60, 60)
 
 w <- imager::width(cropped)
 h <- imager::height(cropped)
 
 m <- convert_imager_to_matrix(cropped)
+
+par(mfrow=c(3,2), mar=c(3,3,2,1))
+plot_rgb_matrix(m,w,h)
+
 cl_m <- clustering::kmean(m, k=4)
 
-par(mfrow=c(2,3), mar=c(3,3,2,1))
-plot_rgb_matrix(m,w,h)
 plot_clustered_rgb_vectors(cl_m, w, h, 1)
 plot_clustered_rgb_vectors(cl_m, w, h, 2)
 plot_clustered_rgb_vectors(cl_m, w, h, 3)
+plot_clustered_rgb_vectors(cl_m, w, h, 4)
 
-cluster <- attr(cl_m, "cluster")
-unique(cluster)
--- vignettes/live_demo/live_demo_specral_and_density_clustering.R
+++ vignettes/live_demo/live_demo_specral_and_density_clustering.R
@@ -1,29 +1,7 @@
-# LIVE DEMONSTRATION
-
 library(clustering)
 
-# generate sample data
-set.seed(188)
-n <- 50
-x <- cbind(
-  x = runif(5, 0, 2) + rnorm(n, sd = 0.05),
-  y = runif(5, 0, 2) + rnorm(n, sd = 0.05)
-)
-x <- t(x)
-d <- c(x, .6, .75, .8, 1.3, 0, 1.6, .6, 1, .9, .9, .5, 1.5, 1, .4, .1, .6);
-x <- matrix(d, nrow=2);
-t <- generate_2d_cluster(100)
-t <- c(t, generate_2d_cluster(100, c(2,2)))
-t <- matrix(t, nrow=2)
 
-two_clusters <- t;
-four_clusters <- x;
-
-par(mfrow=c(3,2), mar=c(3,3,2,1))
-
-## Start
-
-# math utility
+## math utility
 
 x <- c(1,2,3)
 y <- c(4,5,6)
@@ -41,22 +19,26 @@ p8_norm(x)
 infty_distance <- induced_metric_factory(infinity_norm)
 infty_distance(x,y)
 
-# general utility
+## general utility
+par(mfrow=c(2,2), mar=c(3,3,2,1))
+
 single_cluster <- generate_2d_cluster(100)
 plot(single_cluster[1,], single_cluster[2,], pch=20, cex=.5)
 
 nested_clusters <- generate_nested_2d_training_data(400);
 plot(nested_clusters[1,], nested_clusters[2,], pch=20, cex=.5)
 
-# Proximity-based clustering algorithms
+### Proximity-based clustering algorithms
+
+dev.off(); par(mfrow=c(2,2), mar=c(3,3,2,1))
 
 plot(single_cluster[1,], single_cluster[2,])
-# DBSCAN
-dbscan_c <- DBSCAN(single_cluster, .2, 10)
+## DBSCAN
+dbscan_c <- DBSCAN(single_cluster, eps=.2, minPts=10)
 plot_clustered_2d_data(dbscan_c)
 
-# OPTICS
-opt <- OPTICS(single_cluster, .2, 10)
+## OPTICS
+opt <- OPTICS(single_cluster, eps=.2, minPts=10)
 # Reachability Plot
 reachability_plot(opt)
 
@@ -65,4 +47,37 @@ extr_dbscan <- extract_DBSCAN_clustering(opt)
 # extra information can be used to connect points with their predecessors
 plot_clustered_2d_data(extr_dbscan, connect_to_predecessor = TRUE)
 
-# spectral clustering
+## spectral clustering
+
+dev.off(); par(mfrow=c(3,2), mar=c(3,3,2,1))
+
+plot(nested_clusters[1,], nested_clusters[2,], col=c("darkred", "darkblue")[attr(nested_clusters, "innerOrOuter")])
+
+projections <- clustering::spectral_clustering(nested_clusters, k=2)
+plot(projections[1,], projections[2,], col=c("darkred", "darkblue")[attr(nested_clusters, "innerOrOuter")])
+
+clustered <- k_medoids(projections, 2)
+plot_clustered_2d_data(clustered)
+
+attr(nested_clusters, "cluster") <- attr(clustered, "cluster")
+plot_clustered_2d_data(nested_clusters)
+
+cData <- clustering::spectral_clustering(nested_clusters, k=2, mercer_kernel=gaussian_kernel_with_fixed_gamma(30), clustering=k_medoids, 2);
+plot_clustered_2d_data(cData)
+
+
+## Erroneous Input Handling
+
+# incorrect input data layout
+DBSCAN(1:100, .1, 5)
+OPTICS(1:100, .1, 5)
+
+# non-numeric or invalid minPts and epsilon values
+DBSCAN(single_cluster, .2, 1000)
+OPTICS(single_cluster, .2, 10000)
+DBSCAN(single_cluster, -3, 10)
+OPTICS(single_cluster, -3, 10)
+
+# nonsensical projection dimension
+spectral_clustering(nested_clusters, k=3)
+
