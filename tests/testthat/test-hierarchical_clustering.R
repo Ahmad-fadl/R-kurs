56d106f1e484fc6216e3c1c788ade86ecf96977f
Mitodru Niyogi <mitodru.niyogi@gmail.com>
2020-08-21 23:06:03 +0200

    Add more tests

--- tests/testthat/test-hierarchical_clustering.R
+++ tests/testthat/test-hierarchical_clustering.R
@@ -1,6 +1,7 @@
 context("test the Hierarchical clustering algorithm")
 
 data <- distance(USArrests)
+data_1 <- matrix(c(1,1.1,1,1,2,2,2,2.1), ncol=4)
 data2 <- NULL
 data_clust <- hierclust(data, method="single")
 data_clust_complete <- hierclust(data, method="complete")
@@ -15,4 +16,21 @@ test_that("The height of the final cluster tree for single linkage method", {
 
 test_that("wrong input results in error for hierclust", {
   expect_error(hierclust(data2, method="single"))
+})
+
+test_that("wrong input results in error for distance", {
+  expect_error(distance(data2))
+})
+
+test_that("wrong input results in error for hclust_order", {
+  expect_error(hclust_order(data2))
+  })
+
+test_that("Measuring Euclidean distance diagonals using the distance method", {
+  x <- sum(diag(distance(data_1)))
+  expect_equal(0, x)
+})
+
+test_that("Testing the sum of dissimilarities", {
+expect_equal(0.2828427, sum(distance(data_1)))
 })
